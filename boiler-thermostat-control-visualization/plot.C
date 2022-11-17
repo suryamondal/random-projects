03e03190378d59d73e856f47239bceaf628e27fd
Suryanarayan Mondal <suryamondal@gmail.com>
2022-11-17 21:21:43 +0100

    read set temperature

--- boiler-thermostat-control-visualization/plot.C
+++ boiler-thermostat-control-visualization/plot.C
@@ -9,10 +9,10 @@ const TString dayTags[daysInAweek] =
     "Saturday",
     "Sunday" };
 
-const int     binsInAday           = 24. * 60;
+const int     binsInAday           = 24 * 60;
 
 // Day -> Hour, Temperature
-std::map<TString, std::vector<std::tuple<TString,double>>> setTemperature{
+std::map<TString, std::vector<std::tuple<TString,double>>> setTemperature {
   { "Monday",    {std::make_tuple("06:00", 22.),
 		  std::make_tuple("08:30", 18.),
 		  std::make_tuple("12:00", 21.),
@@ -58,6 +58,45 @@ std::map<TString, std::vector<std::tuple<TString,double>>> setTemperature{
 };
 
 
+// simulation parameter
+
+const double waterMass         = 100;	// kg
+const double systemCapacity    = 20;	// kW
+const double heatLoss          = 2;	// kW
+const double heatLoad          = 500;	// kg of air
+const double airSpecificHeat   = 1.005;	// kJ/kg-C
+const double waterSpecificHeat = 4.186; // kJ/kg-C
+const int    simulationCycle   = 10;	// 
+const double outdoorTemperature[24] =	// C
+  {
+    9, // 0
+    9, // 1
+    8, // 2
+    8, // 3
+    8, // 4
+    8, // 5
+    9, // 6
+    9, // 7
+   10, // 8
+   12, // 9
+   15, // 10
+   17, // 11
+   17, // 12
+   18, // 13
+   19, // 14
+   20, // 15
+   19, // 16
+   17, // 17
+   15, // 18
+   14, // 19
+   13, // 20
+   12, // 21
+   11, // 22
+   10  // 23
+  };
+
+
+
 void plot() {
 
   TH2D *temperature_plot = new TH2D("temperature_plot",
@@ -69,6 +108,78 @@ void plot() {
   for(int ij=daysInAweek; ij>0; ij--) {
     temperature_plot->GetYaxis()->SetBinLabel(ij, dayTags[daysInAweek - ij].Data());
   }
+
+  TH2D *water_temperature = (TH2D*)temperature_plot->Clone("water_temperature");
+  water_temperature->SetTitle("Water Temperature Plot in a Week");
+
+
+  // making the set temperature readable
+
+  std::map<TString, std::map<TString,double>> tProgram;
+  for(auto item : setTemperature) {   // map items
+    auto dayName = std::get<0>(item); // TString
+    auto setTemp = std::get<1>(item); // vector
+    for( auto element : setTemp) {    // tuple
+      auto hour = std::get<0>(element);
+      auto temp = std::get<1>(element);
+      (tProgram[dayName])[hour] = temp;
+    }}
+
+
+
+  // simulation
+
+
+  double lst_iT, lst_wT, sT = 0;
+  for(int sim = 0; sim<simulationCycle; sim++) {
+    for(int ijy=daysInAweek; ijy>0; ijy--) {
+      for(int ijx=1; ijx<=binsInAday; ijx++) {
+	double hourAngle = temperature_plot->GetXaxis()->GetBinCenter(ijx);
+
+	// Current status
+
+	double iT = temperature_plot->GetBinContent(ijx, ijy);
+	double wT = water_temperature->GetBinContent(ijx, ijy);
+	if(!iT) {
+	  iT = outdoorTemperature[int(hourAngle)];
+	  temperature_plot-> SetBinContent(ijx, ijy, iT);
+	  water_temperature->SetBinContent(ijx, ijy, wT);
+	  lst_iT = iT; lst_wT = wT; continue;
+	}
+
+	double oT = outdoorTemperature[int(hourAngle)];
+
+	auto search1 = tProgram.find(dayTags[daysInAweek - ijy]);
+	if(search1 != tProgram.end()) {
+	  for(int window = -1; window<=0; window++) {
+	    int day_no = daysInAweek - ijy;
+	    int HH = int(hourAngle);
+	    int mm = int((hourAngle - int(hourAngle)) * 60.) + window;
+	    if(mm < 0) { HH--;    mm += 60; }
+	    if(HH < 0) { day_no--; HH += 24; }
+	    if(day_no < 0) { day_no += daysInAweek;}
+	    TString time_str = TString::Format("%02i:%02i",HH,mm);
+	    auto search11 = tProgram.find(dayTags[day_no]);
+	    if(search11 != tProgram.end()) {
+	      auto search2 = search11->second.find(time_str);
+	      if(search2 != search11->second.end()) {
+		sT = search2->second;
+		std::cout<<" day_str "<<dayTags[day_no]<<" time_str "<<time_str<<" sT "<<sT<<std::endl;
+		break;
+	      }}}
+	}
+
+	if(!sT) {continue;}
+
+	// Projecting in the next bin
+	
+      }
+    }
+  } // for(int sim = 0; sim<simulationCycle; sim++) {
+
+
+
+
 
   temperature_plot->Draw("colz");
   temperature_plot->GetXaxis()->SetNdivisions(-24);
